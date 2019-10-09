9dc356174518969d142b3c535b0c9578898edb9c
Lim changsu <skyisthelimitless@gmail.com>
2019-10-09 20:49:12 +0900

    Added simple express

---
+++ app.js
@@ -0,0 +1,38 @@
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import express from "express";
+import logger from "morgan";
+import * as path from "path";
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// no stacktraces leaked to user unless in development environment
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: app.get("env") === "development" ? err : {}
+  });
+});
+
+module.exports = app;
