5cf0d2820346a27dfe94cfc1d0218bdbf18ab98e
Lim changsu <skyisthelimitless@gmail.com>
2019-10-09 21:05:41 +0900

    Setting sequelize

--- bin/www.js
+++ bin/www.js
@@ -1,4 +1,16 @@
-import app from '../app';
+import http from "http";
+import app from "../app";
+import models from "../models";
 
+var server = http.createServer(app);
 
-
+models.sequelize.sync().then(function() {
+  /**
+   * Listen on provided port, on all network interfaces.
+   */
+  server.listen(port, function() {
+    debug("Express server listening on port " + server.address().port);
+  });
+  server.on("error", onError);
+  server.on("listening", onListening);
+});
